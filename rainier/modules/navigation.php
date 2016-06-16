3f04ddbc5d84ba72d966f9e45a54bb7190a0a422
Matthew Layton <Matthew Layton>
2016-06-16 14:46:48 -0600

    Fixed Rainier navigation

--- rainier/modules/navigation.php
+++ rainier/modules/navigation.php
@@ -1,6 +1,6 @@
 <ul>
-    <li><a href="index.php" title="Home Page">Home</a></li>
-    <li><a href="activities/index.php" title="Activities on Mount Rainier">Activities</a></li>
-    <li><a href="guides/index.php" title="Guides for our Activities on Mount Rainier">Guides</a></li>
-    <li><a href="rentals/controller/index.php" title="Rentals for our Activities on Mount Rainier">Rentals</a></li>
+    <li><a href="/rainier/index.php" title="Home Page">Home</a></li>
+    <li><a href="/rainier/activities/index.php" title="Activities on Mount Rainier">Activities</a></li>
+    <li><a href="/rainier/guides/index.php" title="Guides for our Activities on Mount Rainier">Guides</a></li>
+    <li><a href="/rainier/rentals/controller/index.php" title="Rentals for our Activities on Mount Rainier">Rentals</a></li>
 </ul>
